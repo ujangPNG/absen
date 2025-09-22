49051c296487b9ad2325de7f179d3c94e00413ae
ujang <apasihsh@gmail.com>
2025-09-22 11:59:59 +0700

    robots.txt biar g kenak google bot lg. puki emang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/components/Providers";
 import GA4 from "@/components/GA4"; // Impor dari file .tsx
 import { Suspense } from "react";
 
@@ -35,7 +36,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
